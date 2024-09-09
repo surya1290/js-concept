2c272f257133dc08441b2341fc3db90300031319
1920suryaa <167441461+surya1290@users.noreply.github.com>
2024-09-09 08:44:33 +0000

    datatypes

---
+++ 1basics/01variables.js
@@ -0,0 +1,13 @@
+const accountId = "144553"
+let accountEmail = "surya540@gmail.com"
+var accountPassword = "123445"
+accoutnCity = "jaiput"
+
+// accountId = 2
+
+console.log(accountId);
+accountEmail = "kujut@gmail.com"
+accountPassword = "123221"
+accoutnCity = "bhubaneswar"
+console.table([accountEmail,accountPassword,accoutnCity,accountId])
+console.log(3+3)
---
+++ 1basics/02datatype.js
@@ -0,0 +1,16 @@
+let name = "surya"
+let age = 18
+let isLoggedIn = true
+// power of 2 to 53
+// bigint
+// string
+// boolean
+// null stand alone value
+// indefined not assigned
+// sumbol
+
+
+// object
+console.log(typeof null); //output is object
+console.log(typeof undefined); //output is undefined
+
